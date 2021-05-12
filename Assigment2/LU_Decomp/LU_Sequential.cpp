93898a8fca8af714897200e55546f8a66732d269
Jorge <up201705754@fe.up.pt>
2021-05-12 19:19:30 +0100

    Added PAPI and counting time

--- Assigment2/LU_Decomp/LU_Sequential.cpp
+++ Assigment2/LU_Decomp/LU_Sequential.cpp
@@ -8,13 +8,18 @@
 #include <fstream>
 #include <stdlib.h>
 #include <vector>
+#include <papi.h>
 
 using namespace std;
 
-#define MAX_SIZE = 6000;
+#define MAX_SIZE = 8192;
+
+#define SYSTEMTIME clock_t
+#define FILENAME "exer3.csv"
 
 
 bool matrix_writer(string filename, double matrixToWrite[], int size){
+
     cout << filename;
     ofstream matrixFile;
     matrixFile.open(filename);
@@ -99,20 +104,155 @@ bool lu_splitter(double matrix[], int size){
     return (matrix_writer("l_matrix.csv", l, size) && matrix_writer("u_matrix.csv", u, size) && matrix_writer("result_matrix.csv", matrix, size));
 }
 
+
+double sequential(int size){
+	SYSTEMTIME Time1, Time2;
+
+    double* matrix = new double[size * size];
+    matrix_generator(matrix, size);
+	Time1 = clock();
+    lu_seq(matrix, size);
+    lu_splitter(matrix, size);
+	Time2 = clock();
+
+	return (double)(Time2 - Time1) / CLOCKS_PER_SEC;
+}
+
+void handle_error(int retval)
+{
+	printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
+	exit(1);
+}
+
+void init_papi()
+{
+	int retval = PAPI_library_init(PAPI_VER_CURRENT);
+	if (retval != PAPI_VER_CURRENT && retval < 0)
+	{
+		printf("PAPI library version mismatch!\n");
+		exit(1);
+	}
+	if (retval < 0)
+		handle_error(retval);
+
+	std::cout << "PAPI Version Number: MAJOR: " << PAPI_VERSION_MAJOR(retval)
+			  << " MINOR: " << PAPI_VERSION_MINOR(retval)
+			  << " REVISION: " << PAPI_VERSION_REVISION(retval) << "\n";
+}
+
+
 int main(int argc, char *argv[])
 {
     int size;
     srand(time(NULL));
+	int EventSet = PAPI_NULL;
+
+    int ret;
+	double time;
+	int op;
+	ofstream fileStream;
+	int startSize, endSize, step;
+	int lin, col, nt = 1;
+	long long values[2];
+
+	ret = PAPI_library_init(PAPI_VER_CURRENT);
+	if (ret != PAPI_VER_CURRENT)
+		std::cout << "FAIL" << endl;
+
+	ret = PAPI_create_eventset(&EventSet);
+	if (ret != PAPI_OK)
+		cout << "ERRO: create eventset" << endl;
+
+	ret = PAPI_add_event(EventSet, PAPI_L1_DCM);
+	if (ret != PAPI_OK)
+		cout << "ERRO: PAPI_L1_DCM" << endl;
+
+	ret = PAPI_add_event(EventSet, PAPI_L2_DCM);
+	if (ret != PAPI_OK)
+		cout << "ERRO: PAPI_L2_DCM" << endl;
 
     cout << "Enter matrix size: ";
 
     cin >> size;
 
-    double* matrix = new double[size * size];
-    matrix_generator(matrix, size);
-    
-    lu_seq(matrix, size);
 
-    lu_splitter(matrix, size);
+    	op = 1;
+	do
+	{
+		cout << endl
+			 << "1. Sequential LU Decomposition" << endl;
+		cout << "2. Block LU Decomposition" << endl;
+		cout << "Selection?: [0 to stop] ";
+		cin >> op;
+		if (op == 0)
+		{
+			cout << "Exiting" << endl;
+			break;
+		}
+
+
+		printf("Parameters?: [StartSize] [Step] [EndSize]\n ");
+		cin >> startSize >> step >> endSize;
+
+		int blockSize;
+		if (op == 2){
+			
+			cout << "\n****Block Multiplication****" << endl;
+			cout << "Block size?: [0 < Block Size <= " << startSize << " ]" << endl;
+			cin >> blockSize;
+			if (blockSize <= 0 || blockSize > startSize)
+				exit(8);
+		}
+
+		fileStream.open(FILENAME, fstream::app);
+		do
+		{
+			// Start PAPI counting
+			ret = PAPI_start(EventSet);
+			if (ret != PAPI_OK)
+				cout << "ERRO: Start PAPI" << endl;
+			lin = startSize;
+			col = startSize;
+			switch (op)
+			{
+			case 1:
+				time = sequential(lin);
+				break;
+			case 2:
+				//time = OnMultLine(lin, col);
+				break;
+            }
+			ret = PAPI_stop(EventSet, values);
+			if (ret != PAPI_OK)
+				cout << "ERRO: Stop PAPI" << endl;
+			printf("L1 DCM: %lld \n", values[0]);
+			printf("L2 DCM: %lld \n", values[1]);
+
+			fileStream << startSize << ", " << time << ", " << values[0] << ", " << values[1] << "\n";
+			ret = PAPI_reset(EventSet);
+			if (ret != PAPI_OK)
+				std::cout << "FAIL reset" << endl;
+
+			printf("\nCurrent size: %d\n", startSize);
+			printf("*****************************\n\n");
+
+			startSize += step;
+
+		} while (startSize <= endSize);
+		fileStream.close();
+
+	} while (op != 0);
+
+    	ret = PAPI_remove_event(EventSet, PAPI_L1_DCM);
+	if (ret != PAPI_OK)
+		std::cout << "FAIL remove event" << endl;
+
+	ret = PAPI_remove_event(EventSet, PAPI_L2_DCM);
+	if (ret != PAPI_OK)
+		std::cout << "FAIL remove event" << endl;
+
+	ret = PAPI_destroy_eventset(&EventSet);
+	if (ret != PAPI_OK)
+		std::cout << "FAIL destroy" << endl;
     return 1;
 }
